c05578be24b5c73c743fc512167004a6ef43ff3b
aolyang <aolyang@foxmail.com>
2023-06-01 00:33:20 +0800

    feat(auth): credential login for guest

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,11 +4,11 @@ import Footer from "./footer"
 import type { ReactNode } from "react"
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <main>{children}</main>
+            <Footer/>
+        </>
+    )
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,65 +1,42 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
-// import AppleProvider from "next-auth/providers/apple"
-// import EmailProvider from "next-auth/providers/email"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
-  // https://next-auth.js.org/configuration/providers/oauth
-  providers: [
-    /* EmailProvider({
-         server: process.env.EMAIL_SERVER,
-         from: process.env.EMAIL_FROM,
-       }),
-    // Temporarily removing the Apple provider from the demo site as the
-    // callback URL for it needs updating due to Vercel changing domains
-
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: {
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      },
-    }),
-    */
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET,
-    }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
-  ],
-  theme: {
-    colorScheme: "light",
-  },
-  callbacks: {
-    async jwt({ token }) {
-      token.userRole = "admin"
-      return token
+    theme: {
+        colorScheme: "light",
     },
-  },
+    callbacks: {
+        async jwt({ token }) {
+            token.userRole = "admin"
+            return token
+        }
+    },
+    // https://next-auth.js.org/configuration/providers/oauth
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "guest" },
+                email: { label: "Email", type: "text" }
+            },
+            async authorize(credentials, req) {
+                if (!credentials || !credentials.email || !credentials.username) throw new Error("Invalid Input")
+                return {
+                    username: credentials.username,
+                    email: credentials.email,
+                    id: "-1"
+                }
+            }
+        })
+    ]
+
 }
 
 export default NextAuth(authOptions)
